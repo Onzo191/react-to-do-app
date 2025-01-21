4af49c81de2cc286ea953233a8436b7eb726a7c8
Onzo191 <hungtuan.onzo@gmail.com>
2025-01-21 14:53:40 +0700

    feat(todo): group todos by deadline

--- src/components/Todo/TodoList.component.jsx
+++ src/components/Todo/TodoList.component.jsx
@@ -2,25 +2,50 @@ import TodoItem from "./TodoItem.component";
 import { PropTypes } from "prop-types";
 
 const TodoList = ({ todos, onUpdateTodo, onDeleteTodo }) => {
+  const groupedTodos = todos.reduce((groups, todo) => {
+    const deadline = todo.deadline || "No Deadline";
+    if (!groups[deadline]) {
+      groups[deadline] = [];
+    }
+    groups[deadline].push(todo);
+    return groups;
+  }, {});
+
   return (
     <div>
-      <ul>
-        {todos.map((todo, index) => (
-          <TodoItem
-            key={index}
-            index={index}
-            todo={todo}
-            onUpdateTodo={onUpdateTodo}
-            onDeleteTodo={onDeleteTodo}
-          />
-        ))}
-      </ul>
+      {groupedTodos.length !== 0 ? (
+        Object.keys(groupedTodos).map((deadline) => (
+          <div key={deadline}>
+            <h3>{deadline}</h3>
+            <ul>
+              {groupedTodos[deadline].map((todo, index) => (
+                <TodoItem
+                  key={`${deadline}-${index}`}
+                  index={todos.indexOf(todo)}
+                  todo={todo}
+                  onUpdateTodo={onUpdateTodo}
+                  onDeleteTodo={onDeleteTodo}
+                />
+              ))}
+            </ul>
+          </div>
+        ))
+      ) : (
+        <p>No todos</p>
+      )}
     </div>
   );
 };
 
 TodoList.propTypes = {
-  todos: PropTypes.array.isRequired,
+  todos: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+      deadline: PropTypes.string.isRequired,
+      isComplete: PropTypes.bool.isRequired,
+    })
+  ).isRequired,
   onUpdateTodo: PropTypes.func.isRequired,
   onDeleteTodo: PropTypes.func.isRequired,
 };
